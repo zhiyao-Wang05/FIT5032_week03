1273884b489d224bccec722643cc7b3004915b45
Zhiyao Wang <zwan0225@student.monash.edu>
2020-08-17 17:46:21 +1000

    week03

---
+++ FIT5032_Week03/FIT5032_Week03/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Week03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            ViewBag.Message = "Your application description page.";
+            
+            HelloWorld.Hello hello = new HelloWorld.Hello();
+            
+            ViewBag.Message = hello.GetHello();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
